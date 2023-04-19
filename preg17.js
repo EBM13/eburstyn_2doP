2bae84888450f39ebbbda45181e1838c6bcce5d6
EBM13 <99694977+EBM13@users.noreply.github.com>
2023-04-19 17:07:42 -0500

    commit

---
+++ preg17.js
@@ -0,0 +1,8 @@
+
+let tareas=["Jugar xbox","realizar tarea de programación","nadar"];
+tareas.push("Ir al cine", "Estudiar para Ing. de SW II");// INSERTAR CÓDIGO PARA AGREGAR ELEMENTO AL ARREGLO
+console.log("Mi lista de actividades");
+console.log("************");
+for(let list of tareas){
+    console.log(lista);
+}
